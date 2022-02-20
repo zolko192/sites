c808a5e9871efeda17d7b3b9313cebf077966e0f
zolko192 <zolko192@gmail.com>
2022-02-20 20:24:36 +0100

    uj info

--- My_projects/Python3/youtube_projects/youtube.py
+++ My_projects/Python3/youtube_projects/youtube.py
@@ -1,36 +1,16 @@
 import tkinter as tk
-from pytube import YouTube
 
-root= tk.Tk()
+class Application(object):
+    """ Főosztály létrehozása. """
+    def __init__(self, master = None):
+        self.master = master
+        self.frame = tk.Frame(self.master, width = 600, height = 400, bg = "white")
+        self.frame.pack()
 
-canvas1 = tk.Canvas(root, width = 400, height = 300,  relief = 'raised', bg= 'black')
-canvas1.pack()
 
-label1 = tk.Label(root, text='Gui Youtube Downloader')
-label1.config(font=('helvetica', 14))
-canvas1.create_window(200, 25, window=label1)
-
-label2 = tk.Label(root, text='Enter Video url : ')
-label2.config(font=('helvetica', 10))
-canvas1.create_window(200, 100, window=label2)
-
-entry1 = tk.Entry (root) 
-canvas1.create_window(200, 140, window=entry1)
-
-def download():
-    ytd_url = entry1.get()
-    try:
-        obj = YouTube(ytd_url)
-        filter = obj.streams.filter(progressive=True,file_extension='mp4')
-        filter.get_highest_resolution().download(filename='myvideo.mp4')
-        label3 = tk.Label(root, text= "Downloading Started",font=('helvetica', 10))
-        canvas1.create_window(200, 210, window=label3)
-    except Exception as e:
-            label4 = tk.Label(root, text= "Downloading Failed",font=('helvetica', 10))
-            canvas1.create_window(200, 210, window=label4)
-
-    
-button1 = tk.Button(text='Download', command=download, bg='brown', fg='white', font=('helvetica', 9, 'bold'))
-canvas1.create_window(200, 180, window=button1)
-
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Youtube Downloader")
+    root.geometry("1366x768")
+    app = Application(master=root)
+    root.mainloop()
