997c6c4a16ba67fb0636b45647b8c62ce4135d59
zolko192 <zolko192@gmail.com>
2022-07-11 16:06:50 +0200

    Program 3.0 Edit

--- Program_3.0/scripts.js
+++ Program_3.0/scripts.js
@@ -1,22 +1,3 @@
-function calcAmount() {
-    let price = 1000;
-    let amountInput = document.querySelector("input[name='amount-input']");
-    let amountNumber = parseInt(amountInput.value);
-
-    showSumPrice(price, amountNumber);
-}
-
-function showSumPrice(price, amountNumber) {
-    let showAmount = document.querySelector("span.show-amount");
-    if (amountNumber > 10) {
-        alert("Maximum 10 terméket vásárolhat!");
-        return;
-    }
-    else if (amountNumber < 1) {
-        alert("Minimum 1 terméket kell vásárolnia!");
-    }
-    else {
-        let amount = amountNumber * price;
-        showAmount.innerHTML = amount;
-    }
-}
+let arr = [1,3,5,7];
+arr.push(9);
+console.log(arr);
